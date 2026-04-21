1b2044a19ab08840c62a111dbc6c883491feea87
NayeemIbrahim <nayeemibrahim46@gmail.com>
2026-04-21 20:34:10 +0600

    Include config/database.php in the repository

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Database Configuration
+ * 
+ * This file contains the database connection constants.
+ * It is excluded from version control to protect sensitive data.
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'hk_isp_billing');
