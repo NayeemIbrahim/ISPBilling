7a0244a7210dbb6a955200cab4f65c73f87f86c4
NayeemIbrahim <nayeemibrahim46@gmail.com>
2026-06-02 19:16:29 +0600

    cleanup: disable temporary error reporting

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
-// Enable Error Reporting for Debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Autoloader
 spl_autoload_register(function ($class) {
     // Convert namespace to full file path
